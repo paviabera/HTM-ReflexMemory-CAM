6597152ac242af1d1d4028f59e30bcb80271b0b0
Jennifer Adorno <jadorno@users.noreply.github.com>
2024-08-16 23:49:08 +0000

    rebuild-cleanup

--- .vscode/install-htm-core.sh
+++ .vscode/install-htm-core.sh
@@ -3,5 +3,13 @@ PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 -m ensurep
   && PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 -m pip install setuptools packaging \
   && PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 -m pip install -r htm.core/requirements.txt \
   && cd htm.core \
-  && PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 setup.py install \
+  && PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 -m pip uninstall htm.core \
+  && PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 setup.py install --force \
   && PIP_ROOT_USER_ACTION=ignore LD_LIBRARY_PATH=/usr/local/lib python3.11 -m pip install -r ../.devcontainer/requirements.txt
+
+
+
+
+
+
+
